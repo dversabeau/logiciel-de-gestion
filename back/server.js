3de7677fd3feef6a530868e8bdc5082d1bf9237e
dversabeau <dversabeau@gmail.com>
2022-06-30 12:04:46 +0200

    feature: add back/config

--- back/server.js
+++ back/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require('cors')
-const PORT = process.env.API_PORT
+require('dotenv').config()
+const API_PORT = process.env.API_PORT
+
+
 
 app.listen(API_PORT, () => {
   console.log("le serveur tourne sur le port: " + API_PORT);
