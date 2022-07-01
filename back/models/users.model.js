7d7ac3169c52fd892a65ee3f79aad56e355094bb
mohammad Rahmani <mrahmani788@gmail.com>
2022-07-01 11:11:54 +0200

    fix user model

--- back/models/users.model.js
+++ back/models/users.model.js
@@ -1,6 +1,6 @@
 const query = require("../config/ConnectionDB");
 const AppError = require("../helpers/appError");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs")
 
 module.exports = class User {
 
@@ -24,16 +24,16 @@ module.exports = class User {
     }
     const user = result[0]
     if (user === undefined) {
-      throw new AppError('User was not found', 404)
+      throw new AppError("User was not found", 404)
     }
     return new User(user)
   }
 
   // get All User
   static async getAllUsers() {
-    const result = await query('SELECT USERS.id_user, USERS.role, USERS.username, USERS.email from USERS')
+    const result = await query("SELECT USERS.id_user, USERS.role, USERS.username, USERS.email from USERS")
     if (result[0] === undefined) {
-      throw new AppError('Any User was not found', 404)
+      throw new AppError("Any User was not found", 404)
     }
 
     return result.map(user => new User(user))
@@ -44,12 +44,12 @@ module.exports = class User {
     const checkUser = await query(`select USERS.email from USERS where email = "${email}"`)
 
     if (checkUser[0]) {
-      throw new AppError('cet email dèja existe', 400)
+      throw new AppError("cet email dèja existe", 400)
     }
 
-    await query(`INSERT into USERS SET email = '${email}', password = '${password}', username= '${username}'`)
+    await query(`INSERT into USERS SET email = "${email}", password = "${password}", username= "${username}"`)
 
-    const getUser = await this.getUser('email', email)
+    const getUser = await this.getUser("email", email)
 
     return new User(getUser)
   }
@@ -60,16 +60,16 @@ module.exports = class User {
 
     // check username ou email
     if (!checkUser[0]) {
-      throw new AppError('cet email ou username n"existe pas', 404)
+      throw new AppError("cet email ou username n\'existe pas", 404)
     }
 
     // Check Password
     if (!(await bcrypt.compare(password, checkUser[0].password))) {
-      throw new AppError('password is not correct', 400)
+      throw new AppError("password is not correct", 400)
     }
 
     // Get user by email
-    const getUser = await this.getUser('email', checkUser[0].email)
+    const getUser = await this.getUser("email", checkUser[0].email)
     return new User(getUser)
 
   }
@@ -80,7 +80,7 @@ module.exports = class User {
 
     // check username ou email
     if (!checkUser[0]) {
-      throw new AppError('cet user  n"existe pas', 404)
+      throw new AppError("Ce user n\'existe pas", 404)
     }
 
     return await query(`DELETE FROM USERS WHERE id_user = ${id}`)
