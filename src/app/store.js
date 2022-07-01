5345721ae29df4a17bee4c98b7d4fb4e06583ae4
dversabeau <dversabeau@gmail.com>
2022-07-01 16:23:15 +0200

    pre-pull branch dev

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./features/users/usersSlice";
+import operationReducer from "./features/operations/operationsSlice";
 
 export const store = configureStore({
   reducer: {
     user : userReducer,
-    operations: "",
-    categories: "",
+    operation: operationReducer,
+    category: "",
   }
 })
