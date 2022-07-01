25476e08f6b1231abefb40bb9408b18265e0a6c0
mohammad Rahmani <mrahmani788@gmail.com>
2022-07-01 16:22:31 +0200

    fix

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -5,6 +5,6 @@ import styles from "./dashboard.module.css"
 export default function DashboardPage() {
   const {username} = useSelector((state)=> state.user.user)
   return (
-    <div>tu es {username}</div>
+    <section className={cb(styles.page)}>tu es {username}</section>
   )
 }
